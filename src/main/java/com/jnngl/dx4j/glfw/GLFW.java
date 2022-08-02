6b1118dcdf79a5fefc1141cec366a43baed1a197
JNNGL <jnngles@gmail.com>
2022-08-02 12:38:30 +1100

    Add GLFW errors

--- src/main/java/com/jnngl/dx4j/glfw/GLFW.java
+++ src/main/java/com/jnngl/dx4j/glfw/GLFW.java
@@ -5,6 +5,18 @@ import java.util.Objects;
 
 public class GLFW {
 
+  public static final int GLFW_NO_ERROR            = 0;
+  public static final int GLFW_NOT_INITIALIZED     = 0x00010001;
+  public static final int GLFW_NO_CURRENT_CONTEXT  = 0x00010002;
+  public static final int GLFW_INVALID_ENUM        = 0x00010003;
+  public static final int GLFW_INVALID_VALUE       = 0x00010004;
+  public static final int GLFW_OUT_OF_MEMORY       = 0x00010005;
+  public static final int GLFW_API_UNAVAILABLE     = 0x00010006;
+  public static final int GLFW_VERSION_UNAVAILABLE = 0x00010007;
+  public static final int GLFW_PLATFORM_ERROR      = 0x00010008;
+  public static final int GLFW_FORMAT_UNAVAILABLE  = 0x00010009;
+  public static final int GLFW_NO_WINDOW_CONTEXT   = 0x0001000A;
+
   public static final int GLFW_VERSION_MAJOR         = 3;
   public static final int GLFW_VERSION_MINOR         = 3;
   public static final int GLFW_VERSION_REVISION      = 8;
